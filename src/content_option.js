a7101b572d3e48f0400a340123e73725ab83b779
am <am@kali.am>
2023-09-26 21:21:57 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,171 @@
+import image1 from '/home/am/Documents/Portfolio/react-portfolio/src/assets/images/amm.png';
+const logotext = "Am";
+const meta = {
+    title: "Adharv M",
+    description: "I’m John Doe Mern Stack Developer",
+};
+
+const introdata = {
+    title: "I’m Adharv M",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "Highly motivated MERN stack developer with expertise in React, Node.js, MongoDB, and Express.js. Strong problem-solving skills and a passion for learning. Committed to leveraging technology to simplify daily tasks. Self-driven and confident in facing challenges, both personally and professionally.",
+    your_img_url: {image1},
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Hi Dear Friends,  A highly motivated and skilled MERN stack developer with experience in building fullstack web applications. Proficient in modern JavaScript, including React and Node.js, as well as MongoDB and Express.js. Strong problem - solving skills along with passion for continuous learning and staying up-to-date with the latest technologies.I aim to use my knowledge in technology to reduce human efforts in day-to-day life. As a self-learner, I believe that I can work in any circumstances. This confidence helps me to face challenges in my personal and professional life",
+};
+const worktimeline = [{
+        jobtitle: "Mern Stack Developer Intern",
+        where: "Soften Technology",
+        date: "2022",
+    },
+    {
+        jobtitle: "Mern Stack Developer",
+        where: "Acemoney",
+        date: "2022",
+    },
+//     {
+//         jobtitle: "Designer of week",
+//         where: "ALquds",
+//         date: "2019",
+//     },
+];
+
+const skills = [{
+        name: "HTML",
+        value: 90,
+    },
+    {
+        name: "CSS",
+        value: 70,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "React-Native",
+        value: 60,
+    },
+    {
+        name: "NodeJS",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Experienced UI/UX designer skilled in creating intuitive, visually appealing digital interfaces. Proficient in user research, wireframing, and prototyping to optimize user experiences. Collaborative team player with a keen eye for detail and a commitment to crafting designs that resonate with users. Dedicated to delivering aesthetically pleasing and user-centric solutions.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Experienced React Native developer proficient in creating cross-platform mobile applications. Strong grasp of JavaScript and React Native framework. Dedicated to crafting responsive and user-friendly mobile solutions. Committed to staying updated with the latest mobile development trends.",
+    },
+    {
+        title: "Web App",
+        description: "Experienced MERN stack developer proficient in React, Node.js, MongoDB, and Express.js. A problem-solving enthusiast dedicated to creating efficient web applications. Constantly learning and adapting to the latest technologies to deliver innovative solutions. Committed to streamlining processes and enhancing user experiences.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "adharvm26@gmail.com",
+    YOUR_FONE: "+91 7025990841",
+    // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/AdharvM24",
+    facebook: "https://www.facebook.com/adharv.m.1",
+    linkedin: "https://www.linkedin.com/in/adharv-m-703a2a245",
+    twitter: "https://twitter.com/adharvhh",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
